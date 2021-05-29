97b0e34ef06c8831332b024551d76e0532c88f33
firoznep <firoz.thekhan@gmail.com>
2021-05-29 20:07:13 +0545

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
+import MainHeader from "./components/mainHeader";
 import "./style/app.css";
 
-function App() {
+const App = () => {
   return (
     <>
-      <header className="header--main">
-        <nav className="nav--main">
-          {/* brand*/}
-          <a className="nav__logo" href="#">
-            Chatting App
-          </a>
+      <MainHeader />
 
-          <div className="nav--items">
-            <span className="nav__item">placeholder-1</span>
-            <span className="nav__item">placeholder-2</span>
-
-            <div className="nav__item dropdown">
-              <span className="dropdown__title">Dropdown</span>
-              <div className="dropdown__toggle ">
-                <a className="dropdown__toggle__item" href="#">
-                  drop 1
-                </a>
-                <a className="dropdown__toggle__item" href="#">
-                  drop 2
-                </a>
-                <a className="dropdown__toggle__item" href="#">
-                  drop 3
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <button className="btn btn--signin">SignIn</button>
-        </nav>
-      </header>
+      <div
+        className="placeholder"
+        style={{ height: "2000px", backgroundColor: "yellow" }}
+      >
+        placeholder
+      </div>
     </>
   );
-}
+};
 
 export default App;
